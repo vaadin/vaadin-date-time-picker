b5a3f1ffd10db725e85758ed198a38cfbc417461
J. Diogo Oliveira <jdso@vhelsi.com>
2023-03-21 17:40:46 +0000

    test: make sure no initial validation happens for date-time-picker

--- test/common.js
+++ test/common.js
@@ -1,2 +1,8 @@
 var ua = navigator.userAgent;
 var ie11 = /Trident/.test(ua);
+
+async function nextRender(element) {
+  return new Promise(resolve => {
+    Polymer.RenderStatus.afterNextRender(element, resolve);
+  });
+}
