ff66f6d54d4eebcd0ac6ee410f3c3319c2412906
Sergey Vinogradov <mr.vursen@gmail.com>
2023-04-19 12:45:57 +0300

    remove validation-date-time-picker.html from test-suites.js

--- test/test-suites.js
+++ test/test-suites.js
@@ -2,6 +2,5 @@ window.VaadinDateTimePickerSuites = [
   'basic.html',
   'i18n.html',
   'propagated-properties.html',
-  'validation.html',
-  'validation-date-time-picker.html'
+  'validation.html'
 ];
